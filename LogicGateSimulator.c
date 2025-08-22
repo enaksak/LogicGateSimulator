0bdd8dc8b5028a25e061a6561da02a1caee3428b
Enes AKSAK <enaksak@gmail.com>
2025-08-22 22:15:23 +0300

    added menu and cases

---
+++ LogicGateSimulator.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+    int choice,a,b;
+    while (1)
+    {
+        printf("\n==============================\n");
+        printf("-----LOGIC GATE SIMULATOR-----\n");
+        printf("\n==============================\n");
+        printf("1. AND\n");
+        printf("2. OR\n");
+        printf("3. NOT\n");
+        printf("4. XOR\n");
+        printf("5. NAND\n");
+        printf("6. NOR\n");
+        printf("7. XNOR\n");
+        printf("8.EXIT\n");
+        printf("Choose a Gate: \n");
+        scanf("%d",&choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",AND(a,b));
+            truth_table_2input("AND");
+            break;
+
+        case 2:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",OR(a,b));
+            truth_table_2input("OR");
+            break; 
+
+        case 3:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",NOT(a,b));
+            truth_table_2input("NOT");
+            break;
+
+        case 4:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",XOR(a,b));
+            truth_table_2input("XOR");
+            break;
+
+        case 5:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",NAND(a,b));
+            truth_table_2input("NAND");
+            break;
+
+        case 6:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",NOR(a,b));
+            truth_table_2input("NOR");
+            break;
+
+        case 7:
+            printf("Enter A (0/1)\n");
+            scanf("%d",&a);
+
+            printf("Enter B (0/1)\n");
+            scanf("%d",&b);
+
+            printf("Result: %d\n",XNOR(a,b));
+            truth_table_2input("XNOR");
+            break;
+
+        case 8:
+            exit(0);
+            break;
+
+        default:
+            break;
+        }
+
+
+    }
+    
+
+
+
+
+
+    return 0;
+}
